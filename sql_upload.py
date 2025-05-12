2b01bc73d3286877ccbbb4183eee7f12b330b57d
MargatitaF <557795@student.fontys.nl>
2025-05-12 12:45:25 +0200

    moved my md files to a separate folder

---
+++ sql_upload.py
@@ -0,0 +1,23 @@
+# Standard library imports
+import os
+
+# Database imports
+import psycopg2
+
+
+db_info = os.getenv("DB_INFO")
+if not db_info:
+    raise ValueError("Database not found. Please set the DB_INFO environment variable.")
+
+conn = psycopg2.connect(db_info)
+cur = conn.cursor()
+#------------------------------------------------------------------------------
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+
+cur.execute("SELECT * FROM test;")
+cur.fetchone()
+conn.commit()
+
+cur.close()
+conn.close()
